296f55f905b7096e6b22eadb7599a28557c70f83
Luis Rangel Castro <luisrangelc@rpaipos.com>
2019-08-15 21:30:45 -0600

    Clase04 - funciones

---
+++ clase04.js
@@ -0,0 +1,12 @@
+var nombre = 'Luis'
+    ,edad = 39;
+
+function imprimirEdad(n,e){
+    console.log(`${n} tiene ${e} años`);
+}
+
+imprimirEdad(nombre,edad);
+imprimirEdad('Vicky',28);
+imprimirEdad('Erick',39);
+imprimirEdad('Dario',27);
+imprimirEdad(25,'Carlos');
