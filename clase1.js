6a9cac99c8501cf7d54943c62aed62852c5243ab
Luis Rangel Castro <luisrangelc@rpaipos.com>
2019-08-15 19:53:12 -0600

    Variables

--- clase1.js
+++ clase1.js
@@ -1,1 +1,9 @@
-console.log('');
+console.log('Hello world JS!');
+
+var nombre = 'Luis'
+    ,apellido = 'Rangel'
+    ,edad = 39;
+
+console.log('Hello my name is ' + nombre + ' ' + apellido);
+console.log(`Hello my name is ${nombre} ${apellido}`);
+console.log(`And I am ${(edad)} years old`);
