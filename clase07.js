e1d2d2d424d5d2517b22cdb8f42a2ec7aefa9c84
Luis Rangel Castro <luisrangelc@rpaipos.com>
2019-08-16 20:17:58 -0600

    clase07 desestructurar objetos

---
+++ clase07.js
@@ -0,0 +1,21 @@
+var luis = {
+    first_name: 'Luis',
+    last_name: 'Rangel',
+    age: 39
+};
+
+var david = {
+    first_name: 'David',
+    last_name: 'Castro',
+    age: 25
+};
+
+function imprimirNombreYEdad(persona) {
+	var {first_name, age } = persona;
+	console.log(`Hello, my name is ${first_name} and I'm ${age} years old.`);
+}
+
+imprimirNombreYEdad(luis);
+imprimirNombreYEdad(david);
+
+console.log(Date.now());
