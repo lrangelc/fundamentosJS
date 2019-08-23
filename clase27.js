9aa16f727444245a6cf4009657749481f19a7d05
Luis Rangel Castro <luisrangelc@rpaipos.com>
2019-08-22 23:36:16 -0600

    clase27 Cómo funciona el tiempo en JavaScript

---
+++ clase27.js
@@ -0,0 +1,56 @@
+// console.log('a');
+// console.log('b');
+// console.log('c');
+
+
+// console.log('a');
+// setTimeout(() => {
+//     console.log('b');
+// }, 2000);
+// console.log('c');
+
+
+// function fnB() {
+//     console.log('b');
+// }
+
+// console.log('a');
+// setTimeout(fnB, 2000);
+// console.log('c');
+
+
+// setTimeout(() => {
+//         console.log('d');
+//     }, 2000);
+// for (var i=0;i < 1000000;i++) {
+
+// }
+
+
+// setTimeout(() => {
+//     console.log('a');
+// }, 2000);
+// setTimeout(() => {
+//     console.log('b');
+// }, 1000);
+
+
+// setTimeout(() => {
+//     console.log('x');
+// }, 2000);
+// setTimeout(() => {
+//     console.log('y');
+// }, 2000);
+
+
+setTimeout(() => {
+    console.log('a');
+}, 3000);
+console.log('b');
+for (var i = 0; i < 1000000000; i++) {
+
+}
+console.log('c');
+setTimeout(() => {
+    console.log('d');
+}, 2000);
